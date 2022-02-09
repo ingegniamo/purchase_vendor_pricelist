c4f16129fda7d23c6c324445e817b9156432e961
wahabalimalik <wahabalimalik@gmail.com>
2022-02-09 18:31:47 +0500

    [IMP]change Vedor Pricelist menu to Purchase/Product menu

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Vendor Pricelist",
+
+    'summary': """
+        Adding vendor pricelist view to purchase product menù """,
+
+    'description': """
+        In this Module we Add vendor pricelist view to purchase product menù .
+    """,
+
+    'author': "Wahab Ali Malik",
+    'website': "",
+    'category': 'Inventory/Purchase',
+    'version': '0.1',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_views.xml',
+    ]
+}
